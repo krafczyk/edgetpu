e318952e067e2212d844803353a33329aac299d8
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2020-10-19 09:36:25 -0500

    Only build python 3.8 version. to help speed up compilation

--- scripts/build_swig.sh
+++ scripts/build_swig.sh
@@ -19,7 +19,7 @@ set -x
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly MAKEFILE="${SCRIPT_DIR}/../Makefile"
 readonly DOCKER_CPUS="${DOCKER_CPUS:=k8}"
-PYTHON_VERSIONS="35 36 37 38"
+PYTHON_VERSIONS="38"
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
